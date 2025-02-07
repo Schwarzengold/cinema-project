de0326391e73a51153e7311262e190229584b37b
ilyashag.it@gmail.com <ilyashag.it@gmail.com>
2025-02-07 21:44:26 +0200

    Implemented the data seeder that populates movies, cinema halls, sessions, and seats.

---
+++ backend/Cinema API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Cinema_API.Models;
+
+namespace Cinema_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<CinemaHall> CinemaHalls { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
