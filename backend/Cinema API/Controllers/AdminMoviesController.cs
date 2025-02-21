2388694ebb4a50706f7052852d63230e40a9fc3a
ilyaaaKen <ilyashag.it@gmail.com>
2025-02-21 21:29:06 +0200

    Develop movie CRUD API

---
+++ backend/Cinema API/Controllers/AdminMoviesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cinema_API.Data;
+using Cinema_API.Models;
+
+namespace Cinema_API.Controllers
+{
+    [Route("api/admin/movies")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminMoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public AdminMoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return Ok(movies);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovie([FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+            return Ok(movie);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, [FromBody] Movie updatedMovie)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound();
+
+            movie.Title = updatedMovie.Title;
+            movie.Description = updatedMovie.Description;
+            movie.BannerUrl = updatedMovie.BannerUrl;
+            movie.TrailerUrl = updatedMovie.TrailerUrl;
+            movie.ReleaseDate = updatedMovie.ReleaseDate;
+            await _context.SaveChangesAsync();
+            return Ok(movie);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound();
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+            return Ok(new { Message = "Movie deleted successfully." });
+        }
+    }
+}
